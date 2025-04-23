fd8ab2eaff8639403fcd2f5d0b83498f65dc2f72
prieloizee <priscila.e.paulino@aluno.senai.br>
2025-04-23 16:12:03 -0300

    23/04

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'10.89.240.85',
+    host:'10.89.240.71',
     user:'priscila',
     password:'senai@604',
     database:'projeto_senai'
